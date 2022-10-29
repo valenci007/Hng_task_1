33f3939d4db554ebc1f608901bf8132de997c217
valenci007 <iamaqim@gmail.com>
2022-10-29 20:53:41 +0100

    first commit

---
+++ task_1.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Content-Type: application/json; charset=utf-8');
+$myData = [
+    "slackUsername" => "AQIM",
+    "backend" => true,
+    "age" => 30,
+    "bio" => "My Name is Aqim I am a backend developer I graduated as a Mechanical Engineer but my love for problem solving made me want to be a programmer",
+];
+
+echo json_encode($myData, 200);
+
+?>
