b922dd7fc13de9a34270d66a74386a64a5de9c6b
valenci007 <iamaqim@gmail.com>
2022-11-05 21:25:00 +0100

    fixed username

--- task_2.php
+++ task_2.php
@@ -62,7 +62,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
         }
     
         $myResponse = [
-            'slackUsername' => "AQIM",
+            'slackUsername' => "valenci007",
             'operation_type' => $data['operation_type'],
             'result' => intval($result),
         ];
